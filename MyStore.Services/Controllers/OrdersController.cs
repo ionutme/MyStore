9912c7df1826f48b631d5f2f38e854747bf06f06
Ionuț <ionut.cristea@outlook.com>
2018-01-22 00:14:45 +0200

    Add Orders controller

--- MyStore.Services/Controllers/OrdersController.cs
+++ MyStore.Services/Controllers/OrdersController.cs
@@ -9,5 +9,37 @@ namespace MyStore.Services.Controllers
 {
     public class OrdersController : ApiController
     {
+        private static readonly Dictionary<int, string> Orders = new Dictionary<int, string>();
+
+        public IEnumerable<string> Get()
+        {
+            return Orders.Values;
+        }
+
+        // GET api/orders/5
+        public string Get(int id)
+        {
+            Orders.TryGetValue(id, out var order);
+
+            return order;
+        }
+
+        // POST api/orders
+        public void Post([FromBody]string value)
+        {
+            Orders.Add(Orders.Count, value);
+        }
+
+        // PUT api/orders/5
+        public void Put(int id, [FromBody]string value)
+        {
+            Orders[id] = value;
+        }
+
+        // DELETE api/orders/5
+        public void Delete(int id)
+        {
+            Orders.Remove(id);
+        }
     }
 }
